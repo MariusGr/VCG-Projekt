9053b3159f2a44dd1ca10acc3af22d8188451e68
Marius Gripp <marius.gripp@arcor.de>
2016-06-21 09:28:37 +0200

    Schatten änderung

--- VCG_Raytracer/src/raytracer/Intersection.java
+++ VCG_Raytracer/src/raytracer/Intersection.java
@@ -48,17 +48,19 @@ public class Intersection {
 
 
     public RgbColor getRgbColor() {
-        if(!this.hit) return Raytracer.backgroundColor; // Wenn kein Objekt getroffen wird, wird dieser Wert für den Pixel verwendet --> Hintergrundfarbe der Szene
-        if(this.shadow) return new RgbColor(0.1f,0.1f,0.1f); //Im Schatten ambientes Licht
+        if(!this.hit) return Raytracer.backgroundColor;      // Wenn kein Objekt getroffen wird, wird dieser Wert für den Pixel verwendet --> Hintergrundfarbe der Szene
 
-        Light l = Raytracer.lightList.get(0);           // aktuell betrachtete Lichtquelle
-        Vec3 lPos = l.getPosition();                    // Position des aktuell betrachteten Lichts
+
+        Light l = Raytracer.lightList.get(0);               // aktuell betrachtete Lichtquelle
+        Vec3 lPos = l.getPosition();                        // Position des aktuell betrachteten Lichts
         Vec3 lVector = lPos.sub(interSectionPoint).normalize();            // Vektor von Schnittpunkt zu Lichtquelle
         Vec3 direction = this.inRay.getDirection();
 
         Material m = this.shape.getMaterial();
         RgbColor rgb = m.getColor(l.getColor(), shape.getNormal(interSectionPoint), lVector, direction);
-        rgb = rgb.add(Raytracer.ambientLight);            // Ambientes Licht addieren, wenn es sich um den Pixel eines getroffenen Objekts handelt
+        rgb = rgb.add(Raytracer.ambientLight);               // Ambientes Licht addieren, wenn es sich um den Pixel eines getroffenen Objekts handelt
+
+        if(this.shadow) rgb.add(-0.5f,-0.5f,-0.5f);         //Im Schatten ambientes Licht
 
         return rgb;
     }
